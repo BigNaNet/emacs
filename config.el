2af95bbb673fe144b372c9757e1d1e194e78cac3
BigNaNet <ferreira.d4.r@gmail.com>
2019-07-17 11:50:30 +0100

    Add config visit, transparency func, keybindings and init  behavior

--- config.el
+++ config.el
@@ -1,10 +1,32 @@
-
 (version)
 
 (org-version)
 
 (setq user-full-name "Daniel Ferreira"
       user-mail-address "ferreira.d4.r@gmail.com")
+
+(defun config-visit()
+  "Visits my config file"
+  (interactive)
+  (find-file "~/.emacs.d/config.org"))
+(defun config-reload()
+  "Relads ~/.emacs.d/config.org at runtime"
+  (interactive)
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+(defun drf/append-to-path (path)
+  "Add a path both to the $PATH variable and to Emacs' exec-path."
+  (setenv "PATH" (concat (getenv "PATH") ":" path))
+  (add-to-list 'exec-path path))
+(defun drf/create-frame()
+  (interactive)
+  (make-frame . (transparency 90))
+  (transparency 90))
+(defun drf/init-file(filepath)
+  (find-file filepath))
+
+(global-set-key (kbd "C-c e") 'config-visit)
+(global-set-key (kbd "C-c r") 'config-reload)
+(global-set-key (kbd "C-<f1>") 'drf/create-frame)
 
 (use-package diminish)
 
@@ -13,16 +35,15 @@
 (delete-selection-mode t)
 (setq require-final-newline t)
 (setq-default fill-column 80)
-;;(add-hook 'text-mode-hook 'turn-on-auto-fill)
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+(setq-default indent-tabs-mode nil)
 
 (setq inhibit-startup-message t)
 (setq initial-scratch-message nil)
 (tool-bar-mode -1)
-;; (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (horizontal-scroll-bar-mode -1)
 (fset 'yes-or-no-p 'y-or-n-p)
-
 (show-paren-mode t)
 (setq show-paren-delay 0.0)
 (add-hook 'prog-mode-hook 'linum-mode)
@@ -35,8 +56,6 @@
 
 (global-auto-revert-mode t)
 (substitute-key-definition 'kill-buffer 'kill-buffer-and-window global-map)
-(windmove-default-keybindings)
-(setq windmove-wrap-around t)
 
 (defun reset-text-size ()
   (interactive)
@@ -47,7 +66,7 @@
 (define-key global-map (kbd "C--") 'text-scale-decrease)
 
 (define-key global-map (kbd "M-g") 'goto-line)
-
+(define-key global-map (kbd "C-c s") 'replace-string)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "M-SPC") 'other-frame)
 
@@ -59,21 +78,19 @@
 (eval-after-load "linum"
   '(set-face-attribute 'linum nil :height 125))
 
-(defun drf/append-to-path (path)
-  "Add a path both to the $PATH variable and to Emacs' exec-path."
-  (setenv "PATH" (concat (getenv "PATH") ":" path))
-  (add-to-list 'exec-path path))
-
-(defun drf/init-file(filepath)
-  (find-file filepath))
-
 ; (use-package nord-theme
  ;  :ensure t
  ;  :config (load-theme 'nord t)
 ;;           (setq nord-comment-brightness 20))
+  (defun transparency (value)
+    "Sets the transparency of the frame window. 0=transparent/100=opaque."
+    (interactive "nTransparency Value 0 - 100 opaque:")
+    (set-frame-parameter (selected-frame) 'alpha value))
 
 (use-package zenburn-theme
-  :config (load-theme 'zenburn t))
+  :config
+  (load-theme 'zenburn t)
+  (transparency 90))
 
 (use-package flycheck
   :init
@@ -86,103 +103,114 @@
 )
 
 (use-package smartparens
-  :diminish smartparens-mode
   :config
-  (smartparens-global-mode)
-  (sp-pair "'" nil :unless '(sp-point-after-word-p)))
+  (require 'smartparens-config)
+  (smartparens-global-strict-mode)
+  (show-smartparens-global-mode)
+  (sp-local-pair 'org-mode "$$" "$$"))
 
 (use-package magit
-   :init
-   (drf/append-to-path "/ifs/opt/app/git/2.8.1-gcc/bin"))
+  :bind
+  ("C-x g" . magit-status))
+
+(use-package yaml-mode
+ :config
+ (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode)))
 
 (use-package python-mode)
-(drf/append-to-path "~/.local/bin")
-(add-hook 'python-mode-hook'(lambda () (auto-complete-mode -1)))
-(add-hook 'python-mode-hook'(lambda () (jedi-mode -1)))
+ (drf/append-to-path "/home/daniel/anaconda3/bin/")
 (use-package elpy
-  :config (elpy-enable))
+   :init
+   (elpy-enable))
+   :config
+   (setq python-shell-interpreter "jupyter"
+       python-shell-interpreter-args "-i --simple-prompt")
 
-;(use-package company-jedi)
-;(add-to-list 'company-backends 'company-jedi)
 
-;(add-hook 'python-mode-hook 'jedi:setup)
+ (use-package ein)
 
-(use-package jedi
-  :init
-  (add-hook 'python-mode-hook 'jedi:setup)
-  (add-hook 'python-mode-hook 'jedi:ac-setup))
-  (setq jedi:complete-on-dot t)
+ ;; use flycheck not flymake with elpy
+ (when (require 'flycheck nil t)
+   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
 
-(use-package ein)
-(add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
+   (add-hook 'elpy-mode-hook 'flycheck-mode))
 
-(setq python-shell-interpreter "jupyter"
-      python-shell-interpreter-args "console --simple-prompt"
-      python-shell-prompt-detect-failure-warning nil)
-(add-to-list 'python-shell-completion-native-disabled-interpreters
-             "jupyter")
+ ;; enable autopep8 formatting on save
+ (use-package py-autopep8)
 
-;; use flycheck not flymake with elpy
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
-
-;; enable autopep8 formatting on save
-(use-package py-autopep8)
-
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+ (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 
 (use-package org
    :pin gnu
   :config
   (set-fontset-font "fontset-default" nil (font-spec :size 20 :name "Symbola"))
-  (setq org-ellipsis " ⬎")
-
-(setq org-todo-keywords '((sequence "TODO(t)" "STARTED(s!)" "WAITING(w@)" "|" "DONE(d!)")))
+  (setq org-ellipsis " F")
+;      (setq org-ellipsis " ⬎")
+  ;(setq org-agenda-files (list "~/Scouts/Lobitos/plano.org"))
+  (setq org-todo-keywords '((sequence "TODO(t)" "STARTED(s!)" "WAITING(w@)" "|" "DONE(d!)")))
   (setq org-todo-keyword-faces
-        '(("TODO" . org-warning) ("STARTED" . "yellow") ("WAITING" . "orange")))
+  '(("TODO" . org-warning) ("STARTED" . "yellow") ("WAITING" . "orange")))
   (setq org-src-fontify-natively t)
   (setq org-src-tab-acts-natively t)
   (setq org-src-window-setup 'current-window)
   (setq org-confirm-babel-evaluate nil)
-
-
-  (add-to-list 'org-structure-template-alist
+    (add-to-list 'org-structure-template-alist
                '("el" "#+BEGIN_SRC emacs-lisp\n?\n#+END_SRC"))
     (add-to-list 'org-structure-template-alist
              '("py" "#+BEGIN_SRC python\n?\n#+END_SRC")))
-
-(use-package org-bullets
+ (custom-set-faces '(org-ellipsis ((t (:foreground "gray70" :underline nil)))))
+ (use-package org-bullets
   :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+    (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+ (use-package ox-moderncv
+    :load-path "~/.emacs.d/org-cv/"
+    :init (require 'ox-moderncv))
+
+(setq org-default-notes-file "~/Desktop/Organizer/TODO_list.org")
+(define-key global-map "\C-cc" 'org-capture)
 
 (setq exec-path (append exec-path '("/usr/bin/tex")))
 
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((python . t)
-   (latex . t)))
+   (latex . t)
+   (ditaa . t)))
 
 (require 'ox-beamer)
 
-(use-package ivy
-  :diminish ivy-mode
-  :config
-  (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-count-format "(%d/%d) ")
-  (setq ivy-extra-directories ()))
+(setq jiralib-url "https://servicedesk.mindstorm.vestas.net")
 
-(use-package swiper
+(global-set-key "\C-ca" 'org-agenda)
+
+(use-package counsel
+  :init
+  (ivy-mode 1)
+  (counsel-mode)
+  :config
+  (setq ivy-use-virtual-buffers t
+        ivy-count-format "(%d/%d) "
+        ivy-wrap t
+        ivy-extra-directories nil
+        ivy-initial-inputs-alist nil
+        ivy-format-function 'ivy-format-function-arrow)
+  (when window-system
+    (setq ivy-height 25))
+  (use-package smex)
   :bind
-  ("C-s" . swiper)
-  ("C-r" . swiper))
+  ("C-x B" . ivy-switch-buffer-other-window)
+  ("C-s" . swiper))
+
+(use-package avy
+  :bind ("C-r" . avy-goto-word-1))
 
 (use-package company
-  :config (global-company-mode t)
-  (setq company-idle-delay 0)
+  :config
+  (global-company-mode t)
+  (setq company-idle-delay 0.1)
   (setq company-minimum-prefix-length 3)
-  (define-key company-active-map (kbd "<tab>") 'company-complete)
+  (define-key company-active-map (kbd "C-j") 'company-complete-selection)
+  (define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous))
 
@@ -208,13 +236,15 @@
  (lambda ()
    (setq pcomplete-cycle-completions nil)))
 
-(find-file "~/Desktop/Jira_Tasks/what_to_do.org")
+(find-file "~/Desktop/Organizer/TODO_list.org")
     ;; fullscreen by default
     (defun fullscreen ()
            (interactive)
            (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
                                 '(2 "_NET_WM_STATE_FULLSCREEN" 0)))
     (fullscreen)
+(setq display-time-format "%k:%M %a %d %b")
+
 (defface egoge-display-time
       '((((type x w32 mac))
       ;; #060525 is the background colour of my default face.
@@ -226,8 +256,28 @@
  ;; This causes the current time in the mode line to be displayed in
  ;; `egoge-display-time-face' to make it stand out visually.
  (setq display-time-string-forms
-       '((propertize (concat " " 24-hours ":" minutes " ")
+       '((propertize (concat  " " 24-hours ":" minutes " " dayname " " day " " monthname " ")
                     'face 'egoge-display-time)))
  (display-time-mode 1)
 
 (setq )
+
+(use-package minions
+  :config
+  (setq minions-mode-line-lighter ""
+        minions-mode-line-delimiters '("" . ""))
+  (minions-mode 1))
+
+(when window-system
+  (use-package moody
+    :config
+    (setq moody-mode-line-height 25
+          x-underline-at-descent-line t)
+    (moody-replace-mode-line-buffer-identification)
+    (moody-replace-vc-mode)))
+
+(when window-system
+  (use-package beacon
+  :config
+  (beacon-mode 1)
+  (setq beacon-blink-when-window-scrolls nil)))
--- init.el
+++ init.el
@@ -1,44 +1,31 @@
 (require 'package)
-
-(setq package-enable-at-startup nil)
-
-(add-to-list 'package-archives
-             '("melpa" . "https://stable.melpa.org/packages/") t)
-
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;; Bootstrap 'use-package'
-(unless (package-installed-p 'use-package)
-	(package-refresh-contents)
-	(package-install 'use-package))
+;; Bootstrap use-package
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
 
+(require 'use-package-ensure)
 (setq use-package-always-ensure t)
 
-;; auto update packages
 (use-package auto-package-update
-  :ensure t
   :config
-  (setq auto-package-update-delete-old-versions t)
-  (setq auto-package-update-hide-results t)
   (auto-package-update-maybe))
 
-
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(org-babel-load-file "~/.emacs.d/config.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "yellow green" "#DC8CC3" "khaki" "#DCDCCC"])
- '(magit-git-executable "git")
  '(package-selected-packages
    (quote
-    (flycheck python-mode company-jedi jedi diredful zenburn-theme ox-beamer use-package swiper smartparens py-autopep8 pdf-tools org-bullets nord-theme magit elpy ein diminish auto-package-update))))
+    (zenburn-theme yaml-mode use-package smex smartparens request-deferred python-mode py-autopep8 pdf-tools org-jira org-bullets nord-theme multiple-cursors multi moody minions magit-popup magit ledger-mode jedi helm graphql ghub flycheck excorporate elpy ein diredful diminish counsel company-jedi beacon auto-package-update ace-window ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(dired-header ((t (:foreground "khaki"))))
- '(font-lock-function-name-face ((t (:foreground "yellow green")))))
+ '(org-ellipsis ((t (:foreground "gray70" :underline nil)))))
